a5d43828b34eb7a49e935475e13ee44990c0679b
ejimoforwilson <ejimoforwilson@gmail.com>
2025-11-05 11:27:23 -0800

    temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,2 @@
 document.getElementById("year").textContent = new Date().getFullYear();
-<<<<<<< HEAD
 document.getElementById("lastModified").textContent = document.lastModified;
-=======
-document.getElementById("lastModified").textContent = document.lastModified;
->>>>>>> ac45e4826ead8f9a155932892f0d20b43812d259
