7434c456c8bbf5dfd6ec41a2c31f4dc7b758c782
ejimoforwilson <ejimoforwilson@gmail.com>
2025-11-13 15:04:16 -0800

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,8 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
