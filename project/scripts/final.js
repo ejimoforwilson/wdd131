2403c251a9fe36d2ecff3926ac43f822e77f8b36
ejimoforwilson <ejimoforwilson@gmail.com>
2025-11-22 14:42:26 -0800

    project

--- project/scripts/final.js
+++ project/scripts/final.js
@@ -29,9 +29,10 @@ bookmarks.forEach(dish => {
   li.textContent = dish;
   bookmarkList.appendChild(li);
 });
-document.querySelector("form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Thank you for contacting us!");
-});
-
+ document.getElementById("contactForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      alert("Thank you for contacting us, " + document.getElementById("name").value + "!");
+      // Optional: reset form
+      this.reset();
+    });
 
